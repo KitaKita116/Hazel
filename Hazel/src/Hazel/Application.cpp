90fb699a7e320d45e80463f1c39a161fd11135dc
keix <https://github.com/Kexi116/Kexi116.github.io.git.com>
2022-11-15 19:25:16 +0800

    basic event and callback:

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -1,15 +1,35 @@
+#include "hzpch.h"
 #include "Application.h"
+
+#include "Hazel/Events/ApplicationEvent.h"
+#include "Hazel/Events/KeyEvent.h"
+#include "Hazel/Events/MouseEvent.h"
+#include "Hazel/Log.h"
+
+#include <GLFW/glfw3.h>
 
 namespace Hazel
 {
+#define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
 	Application::Application()
 	{
+		m_Window = std::unique_ptr<Window>(Window::Create());
+		m_Window->SetEventCallback(BIND_EVENT_FN(OnEvent));
 	}
 	Application::~Application()
 	{
 	}
+	void Application::OnEvent(Event& e)
+	{
+		HZ_CORE_INFO("{0}", e);
+	}
 	void Application::Run()
 	{
-		while (true);
+		while (m_Running)
+		{
+			glClearColor(1.0, 0.0, 0.0, 1.0);
+			glClear(GL_COLOR_BUFFER_BIT);
+			m_Window->OnUpdate();
+		}
 	}
 }
