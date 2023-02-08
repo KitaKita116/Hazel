b1dc60ed5a5fa4a062651a20bf619efef2ccf2d7
keix <https://github.com/Kexi116/Kexi116.github.io.git.com>
2023-02-08 17:19:21 +0800

    Refactored Input

--- Hazel/src/Hazel/Core/Input.h
+++ Hazel/src/Hazel/Core/Input.h
@@ -8,34 +8,11 @@ namespace Hazel {
 
 	class Input
 	{
-	protected:
-		Input() = default;
 	public:
-		virtual ~Input() = default;
-
-		Input(const Input&) = delete;
-		Input& operator=(const Input&) = delete;
-
-	public:
-		//判断按键是否按下
-		inline static bool IsKeyPressed(KeyCode keycode) { return s_Instance->IsKeyPressedImpl(keycode); }
-		//判断鼠标是否按下
-		inline static bool IsMouseButtonPressed(MouseCode button) { return s_Instance->IsMouseButtonPressedImpl(button); }
-		//获取鼠标位置
-		inline static std::pair<float, float> GetMousePosition() { return s_Instance->GetMousePositionImpl(); }
-		inline static float GetMouseX() { return s_Instance->GetMouseXImpl(); }
-		inline static float GetMouseY() { return s_Instance->GetMouseYImpl(); }
-	protected:
-		//实现单独做，因为不同平台使用的方法可能都不一样
-		virtual bool IsKeyPressedImpl(KeyCode keycode) = 0;
-		virtual bool IsMouseButtonPressedImpl(MouseCode button) = 0;
-		virtual std::pair<float, float> GetMousePositionImpl() = 0;
-		virtual float GetMouseXImpl() = 0;
-		virtual float GetMouseYImpl() = 0;
-
-		static Scope<Input> Create();
-	private:
-		static Scope<Input> s_Instance;
+		static bool IsKeyPressed(KeyCode keycode);
+		static bool IsMouseButtonPressed(MouseCode button);
+		static std::pair<float, float> GetMousePosition();
+		static float GetMouseX();
+		static float GetMouseY();
 	};
-
 }
