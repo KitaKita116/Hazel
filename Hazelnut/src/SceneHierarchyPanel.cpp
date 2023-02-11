45ab8b423d63e3785a83c2361e88ec168540afd8
keix <https://github.com/Kexi116/Kexi116.github.io.git.com>
2023-02-11 20:38:54 +0800

    add Properties panel

--- Hazelnut/src/SceneHierarchyPanel.cpp
+++ Hazelnut/src/SceneHierarchyPanel.cpp
@@ -1,5 +1,7 @@
 #include "SceneHierarchyPanel.h"
 #include <imgui/imgui.h>
+
+#include <glm/gtc/type_ptr.hpp>
 #include "Hazel/Scene/Components.h"
 
 namespace Hazel
@@ -23,7 +25,16 @@ namespace Hazel
 				Entity e(entityID, m_Context.get());
 				DrawEntityNode(e);
 			}
+
 		);
+		//按下鼠标左键且在窗口上
+		if (ImGui::IsMouseDown(0) && ImGui::IsWindowHovered())
+			m_SelectionContext = {};
+		ImGui::End();
+
+		ImGui::Begin("Properties");
+		if (m_SelectionContext)
+			DrawComponents(m_SelectionContext);
 
 		ImGui::End();
 	}
@@ -35,7 +46,7 @@ namespace Hazel
 		ImGuiTreeNodeFlags flags = ((m_SelectionContext == entity) ? ImGuiTreeNodeFlags_Selected : 0) | ImGuiTreeNodeFlags_OpenOnArrow;
 		//返回值表明当前树节点是否被打开
 		bool opened = ImGui::TreeNodeEx((void*)entity.getEntityID(), flags, tag.c_str());
-		
+
 		if (ImGui::IsItemClicked())
 		{
 			m_SelectionContext = entity;
@@ -48,6 +59,39 @@ namespace Hazel
 			if (opened)
 				ImGui::TreePop();
 			ImGui::TreePop();
+		}
+	}
+
+	void SceneHierarchyPanel::DrawComponents(Entity entity)
+	{
+		if (entity.HasComponent<TagComponent>())
+		{
+			auto& tag = entity.GetComponent<TagComponent>().Tag;
+
+			char buffer[256];
+			memset(buffer, 0, sizeof(buffer));
+			strcpy_s(buffer, sizeof(buffer), tag.c_str());
+
+			ImGui::Text("Tag");
+			ImGui::SameLine();
+
+			if (ImGui::InputText(" ", buffer, sizeof(buffer)))
+			{
+				tag = std::string(buffer);
+			}
+		}
+
+		if (entity.HasComponent<TransformComponent>())
+		{
+			if (ImGui::TreeNodeEx((void*)typeid(TransformComponent).hash_code(), ImGuiTreeNodeFlags_DefaultOpen, "Transform"))
+			{
+				auto& transform = entity.GetComponent<TransformComponent>().Transform;
+				ImGui::Text("Position");
+				ImGui::SameLine();
+				ImGui::DragFloat3(" ", glm::value_ptr(transform[3]), 0.1f);
+
+				ImGui::TreePop();
+			}
 		}
 	}
 }
